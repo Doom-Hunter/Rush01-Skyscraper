c386d38d2956d9bb2612b5d9d94731e0e26bf91c
Tharr Nay Minn <tnay-min@c2r1s14.42singapore.sg>
2026-06-07 23:34:57 +0800

    Fixed bonus

--- ft_solve_skyscraper.c
+++ ft_solve_skyscraper.c
@@ -6,7 +6,7 @@
 /*   By: tnay-min <tnay-min@student.42singapore.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/07 19:54:29 by tnay-min          #+#    #+#             */
-/*   Updated: 2026/06/07 22:40:04 by tnay-min         ###   ########.fr       */
+/*   Updated: 2026/06/07 23:28:06 by tnay-min         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,35 @@ int	ft_is_used(unsigned short *perm, int index, unsigned short current)
 	return (0);
 }
 
+int	ft_is_solved(unsigned short **board, int size)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < size)
+	{
+		y = 0;
+		while (y < size)
+		{
+			if (ft_number_from_mask(board[x][y]) < 0)
+				return (0);
+			y++;
+		}
+		x++;
+	}
+	return (1);
+}
+
 int	ft_solve_skyscraper(unsigned short **board, int *clues, int size)
 {
 	int	i;
-	int	loop;
+	int	solved;
+	int	hard_limit;
 
-	loop = 0;
-	while (loop < size)
+	hard_limit = 100;
+	solved = 0;
+	while (!solved && hard_limit > 0)
 	{
 		i = 0;
 		while (i < size)
@@ -45,7 +67,8 @@ int	ft_solve_skyscraper(unsigned short **board, int *clues, int size)
 			ft_permute_col(board, clues, size, i);
 			i++;
 		}
-		loop++;
+		solved = ft_is_solved(board, size);
+		hard_limit--;
 	}
 	return (0);
 }
